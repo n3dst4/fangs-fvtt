cae3b24791067ea5a4db4b27567d0e7331fb3225
Neil de Carteret <git@lumphammer.net>
2022-09-29 18:50:54 +0100

    latest

--- module/fangs.js
+++ module/fangs.js
@@ -13,8 +13,32 @@ Hooks.once("init", async function () {
 });
 
 Hooks.once("ready", () => {
+  // $("html").css("font-size", "18px");
 });
 
 Hooks.on("renderActorSheet", (app, html, data) => {
   html.find("[data-roll-attribute=\"attune\"]").html("Awareness");
+  const img = html.find("#name-alias > img");
+  const src = img.attr("src");
+  img.remove();
+  const newDiv = $('<div id="portrait"/>');
+  newDiv.css("background-image", `url(${src})`);
+  html.find("#name-alias").prepend(newDiv);
+  newDiv.on("click", () => {
+    new FilePicker({
+      type: "image",
+      callback: (path) => {
+        app.actor.update({img: path});
+        // game.settings.set(moduleName, customPathKey, path);
+        // newDiv.css("background-image", `url(${path})`);
+      },
+      top: app.position.top + 40,
+      left: app.position.left + 10,
+    }).browse(src);
+    // new ImagePopout(src, {
+    //   title: data.name,
+    //   shareable: true,
+    //   uuid: data.uuid
+    // }).render(true);
+  });
 });
