154b469579b24017406db8e65f0736c5d8cb4d15
Neil de Carteret <git@lumphammer.net>
2022-09-30 18:59:18 +0100

    latest

--- module/fangs.js
+++ module/fangs.js
@@ -1,44 +1,33 @@
-
 const moduleName = "louder-whispers";
 const no = "No";
 const yesTemp = "Yes (temporary)";
 const yesPerm = "Yes (permanent until dismissed)";
-const showWhisperNotificationsKey = "showWhisperNotifications" ;
+const showWhisperNotificationsKey = "showWhisperNotifications";
 const overrideAudioKey = "overrideAudioKey";
 const enhanceMessageKey = "enhanceMessage";
-const customPathKey = "customPath"
+const customPathKey = "customPath";
 const notifChoices = [no, yesTemp, yesPerm];
 
-Hooks.once("init", async function () {
-});
+Hooks.once("init", async function () {});
 
-Hooks.once("ready", () => {
-  // $("html").css("font-size", "18px");
-});
+Hooks.once("ready", () => {});
 
 Hooks.on("renderActorSheet", (app, html, data) => {
-  html.find("[data-roll-attribute=\"attune\"]").html("Awareness");
+  html.find('[data-roll-attribute="attune"]').html("Awareness");
   const img = html.find("#name-alias > img");
   const src = img.attr("src");
   img.remove();
-  const newDiv = $('<div id="portrait"/>');
-  newDiv.css("background-image", `url(${src})`);
-  html.find("#name-alias").prepend(newDiv);
-  newDiv.on("click", () => {
-    new FilePicker({
-      type: "image",
-      callback: (path) => {
-        app.actor.update({img: path});
-        // game.settings.set(moduleName, customPathKey, path);
-        // newDiv.css("background-image", `url(${path})`);
-      },
-      top: app.position.top + 40,
-      left: app.position.left + 10,
-    }).browse(src);
-    // new ImagePopout(src, {
-    //   title: data.name,
-    //   shareable: true,
-    //   uuid: data.uuid
-    // }).render(true);
-  });
+  const portraitDiv = $('<div id="portrait"/>')
+    .css("--portrait-image", `url("${getRoute(src)}")`)
+    .on("click", () => {
+      new FilePicker({
+        type: "image",
+        callback: (path) => {
+          app.actor.update({ img: path });
+        },
+        top: app.position.top + 40,
+        left: app.position.left + 10,
+      }).browse(src);
+    });
+  html.find("#name-alias").prepend(portraitDiv);
 });
